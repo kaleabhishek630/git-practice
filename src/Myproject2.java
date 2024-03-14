9276e0079ca1d07937aa31ec743f1e4a0b1edbb4
Abhishek <kaleabhishek630@gmail.com>
2024-03-14 12:03:15 +0530

    MyProject2 added

---
+++ src/Myproject2.java
@@ -0,0 +1,11 @@
+
+public class Myproject2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String t= "this is my second project ";
+       System.out.println("title :"+t);
+       
+	}
+
+}
