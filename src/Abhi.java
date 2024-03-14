f79ebf048255522fadb288e542969aac6efade17
Abhishek <kaleabhishek630@gmail.com>
2024-03-14 11:36:16 +0530

    third changed

---
+++ src/Abhi.java
@@ -0,0 +1,12 @@
+
+public class Abhi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("my name is smaran");
+     System.out.println(" from gujrat");
+     System.out.println(" from maharash");
+ 
+	}
+
+}
---
+++ src/Program2.java
@@ -0,0 +1,12 @@
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a="Abhishek";
+		System.out.println("Name :"+ a);
+
+	}
+
+}
